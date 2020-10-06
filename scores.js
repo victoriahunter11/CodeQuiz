ed288ce9fbb1cf84a72114a6200400220c3e82bd
Victoria Hunter <victoria.jane09@gmail.com>
2020-10-06 14:48:28 -0400

    correct answer or incorrect answer

--- scores.js
+++ scores.js
@@ -1,15 +1,15 @@
 
 var username = document.getElementById("username");
 var saveScoreBtn = document.getElementById("saveScoreBtn");
-var finalScore = document.getElementById("finalScore");
-var mostRecentScore = localStorage.getItem("mostRecentScore");
+var finalScore = document.getElementById('finalScore');
+var mostRecentScore = localStorage.getItem('mostRecentScore');
 var score = localStorage.getItem('score');
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 console.log("highScores");
 
 
-finalScore.innerText = (mostRecentScore);
+finalScore.innerText = mostRecentScore;
 
 
 username.addEventListener("keyup" , () => {
